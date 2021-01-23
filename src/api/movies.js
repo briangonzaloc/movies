b48760a175e566510f24967ed8589e5f7009f69b
Brian Gonzalo Cespedes <briancespedes94@gmail.com>
2021-01-23 01:15:11 -0300

    Filter by query

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,3 +1,7 @@
 export const getRecomendedMovies = () => {
-  return fetch(`${process.env.REACT_APP_API_URL}/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc`);
+  return fetch(`${process.env.REACT_APP_API_URL}/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc`);
+}
+
+export const searchMovies = query => {
+  return fetch(`${process.env.REACT_APP_API_URL}/search/movie?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc&query=${query}`);
 }
--- src/components/StarList/index.js
+++ src/components/StarList/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StarIcon from '../Icons/Star';
 
 const StarList = ({count = 5}) => {
-  return [...Array(count)].map( (i, index) => <StarIcon key={index} onClick={event => { event.preventDefault(); }}/>);
+  const [ starSelected, setStarSelected] = useState(null);
+  
+  return [...Array(count)].map( (i, index) => ( 
+    <StarIcon 
+      key={index} 
+      selected = { starSelected && index <= starSelected}
+      onClick={event => { 
+        event.preventDefault();
+        if( starSelected === index ){
+          setStarSelected(null)
+        }else{
+          setStarSelected(index);
+        }
+      }
+    }/>
+  ));
 }
 
 export default StarList;
